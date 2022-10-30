013d15133f2c5ca64adb26799dc1ad71e1026b09
mneiracarpio <marco.neira.carpio@gmail.com>
2022-10-29 22:07:56 -0600

    First Commit
    Implementation of packages: dataacces, models, services, servlets. JSP unfinished.

---
+++ build/web/WEB-INF/users.jsp
@@ -0,0 +1,49 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix = "fmt" uri = "http://java.sun.com/jsp/jstl/fmt" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Users</title>
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>User Management System</h1>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>E-Mail</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Active</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <c:forEach var="user" items="${users}">
+                                <tr>
+                                    <td>${user.email}</td>
+                                    <td>${user.firstName}</td>
+                                    <td>${user.lastName}</td>
+                                    <td>${user.active ? "Y" : "N"}</td>
+                                    <td>
+                                        <a href="">Edit</a>
+                                        <a href="">Delete</a>
+                                    </td>
+                                </tr>
+                            </c:forEach>
+
+                        </tbody>
+                    </table>
+                 </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ src/java/ca/sait/lab6/dataaccess/UserDB.java
@@ -0,0 +1,145 @@
+package ca.sait.lab6.dataaccess;
+
+import ca.sait.lab6.models.Role;
+import ca.sait.lab6.models.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserDB {
+
+    public List<User> getAll() throws Exception {
+        List<User> users = new ArrayList<>();
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String sql = "SELECT email, active, first_name, last_name, password, role_id, role_name FROM user a, role b WHERE a.role = b.role_id";
+        
+        try {
+            
+            // with prepare statement
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            // with create statement
+            //rs = con.createStatement().executeQuery(sql);
+            while (rs.next()) {
+                String email = rs.getString("email");
+                boolean active = rs.getBoolean("active");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                String password = rs.getString("password");
+                int roleId = rs.getInt("role_id");
+                String roleName = rs.getString("role_name");
+                
+                Role role = new Role(roleId, roleName);
+                User user = new User(email, active, firstName, lastName, password, role);
+                
+                users.add(user);
+                
+            }
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+
+        return users;
+    }
+
+    public User get(String email) throws Exception {
+        User user = null;
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = "SELECT active, first_name, last_name, password, role_id, role_name FROM user a, role b WHERE a.role = b.role_id AND email=? LIMIT 1";
+        
+        try {
+            boolean active = rs.getBoolean("active");
+            String firstName = rs.getString("first_name");
+            String lastName = rs.getString("last_name");
+            String password = rs.getString("password");
+            int roleId = rs.getInt("role_id");
+            String roleName = rs.getString("role_name");
+
+            Role role = new Role(roleId, roleName);
+            user = new User(email, active, firstName, lastName, password, role);             
+                
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+        
+        return user;
+    }
+
+    public boolean insert(User user) throws Exception {
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        String sql = "INSERT INTO user (email, first_name, last_name, password, role ) " + 
+                     "VALUES (?, ?, ?, ?, ?)";
+        boolean inserted;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getFirstName());
+            ps.setString(3, user.getLastName());
+            ps.setString(4, user.getPassword());
+            ps.setInt(5, user.getRole().getId());
+            inserted = (ps.executeUpdate() != 0);
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+        
+        return inserted;
+    }
+
+    public boolean update(User user) throws Exception {
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        String sql = "UPDATE user SET first_name=?, last_name=?, password=?, role=? WHERE email=?";
+        boolean updated;
+        try {
+            ps = con.prepareStatement(sql);
+            //ps.setString(1, user.getEmail());
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setString(3, user.getPassword());
+            ps.setInt(4, user.getRole().getId());
+            ps.setString(5, user.getEmail());
+            updated = (ps.executeUpdate() != 0);
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+        return updated;
+    }
+
+    public boolean delete(User user) throws Exception {
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        String sql = "DELETE FROM user WHERE email=?";
+        boolean deleted;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, user.getEmail());
+            deleted = ps.executeUpdate() != 0;
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+        
+        return deleted;
+    }
+
+}
